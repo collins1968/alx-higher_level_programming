ee54392f03cbf95548f920b0f3b07237abea83a7
collins1968 <collinsmwendwa1968@gmail.com>
2023-04-12 06:58:16 +0000

    python input output

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Class to Object Module'''
+
+
+def class_to_json(obj):
+    '''
+    function that returns the dict
+    description with simple data
+    structure
+    '''
+    return obj.__dict__
